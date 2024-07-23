e45875c560c8c52e57792d924b14495623355370
eric-crump <121046143+eric-crump@users.noreply.github.com>
2024-07-22 21:07:36 -0400

    test

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,16 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
     const [files, setFiles] = useState([]);
+    const [uploadedImage, setImage] = useState([]);
 
     const getContent = async () => {
         // const asset = await Stack.getImage('bltc6d770f343361373');
         // console(asset);
         // let result = await fetch('/api/images/foo');
-        
+
         // const data = await result.json();
         // console.log('result', data.theResult);
-         console.log(process.env.CONTENTSTACK_MANAGEMEMENT_TOKEN, process.env.CONTENTSTACK_API_KEY)
+        console.log(process.env.CONTENTSTACK_MANAGEMEMENT_TOKEN, process.env.CONTENTSTACK_API_KEY)
 
         let result = await fetch('https://api.contentstack.io/v3/assets/bltc6d770f343361373', {
             method: "GET",
@@ -21,10 +22,10 @@ export default function Home() {
                 "Content-Type": "application/json"
             }
         })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+            })
     }
 
     useEffect(() => {
@@ -32,9 +33,10 @@ export default function Home() {
     }, []);
 
 
-    async function upload(){
+    async function upload() {
         const data = new FormData();
-        data.append('upload', files[0]);
+        console.log('the file', uploadedImage)
+        data.append('asset[upload]', uploadedImage, 'test.svg');
         let result = await fetch('https://api.contentstack.io/v3/assets/', {
             method: "POST",
             body: data,
@@ -44,10 +46,9 @@ export default function Home() {
                 "Content-Type": "multipart/form-data"
             }
         })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-        })
+            .then((response) => response.text())
+            .then((result) => console.log(result))
+            .catch((error) => console.error(error));
     }
 
     return (
@@ -56,26 +57,27 @@ export default function Home() {
                 type="file"
                 onChange={(e) => {
                     console.log(e.target.files);
-                    let seletedFiles = e.target.files;
-                    if (seletedFiles && seletedFiles[0]) {
-                        let blobUrl = URL.createObjectURL(seletedFiles[0]);
+                    let selectedFiles = e.target.files;
+                    if (selectedFiles && selectedFiles[0]) {
+                        let blobUrl = URL.createObjectURL(selectedFiles[0]);
                         let temp = [...files, blobUrl]
                         setFiles(temp);
+                        setImage(e.target.files[0]);
                     }
                 }}
             />
-            
+
             <div>
                 <button className="border py-1 px-3 mt-3" onClick={upload}>
                     Upload
                 </button>
             </div>
-            
+
 
             {files.map((file, index) => (
                 <img key={index} src={file} />
             ))}
-            
+
         </div>
     );
 }
